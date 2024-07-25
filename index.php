89c0861e02bad85307908162153c1817f37afb33
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-07-25 09:30:08 +0330

    fix loader bug

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
 
-if(isset($_GET['loadbalance']) and $_GET['loadbalance'] != 'true') {
-    return false;
+if(isset($_GET['loadbalance']) and $_GET['loadbalance'] == 'true') {
+   require __DIR__ . '/loader.php';
 }
 
-
-define('DOC_ROOT', __DIR__);
-
-
-//var_dump($_REQUEST);
-require __DIR__ . "/classes/BaseClass.php";
-
-//die(__DIR__ . "/classes/{$action}/index2.php");
-
-$action = ucfirst($_REQUEST['action']);
-//var_dump($action);
-
-
-//die(__DIR__ . "/classes/".$action."/index.php");
-require __DIR__ . "/classes/".$action."/index.php";
-
-$class = new $action($_REQUEST);
-
-$class->run();
-
-
-exit;
