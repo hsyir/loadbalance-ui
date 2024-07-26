2129e138e21badbc10cd843b1f22e1ede894c815
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-07-26 14:01:22 +0330

    build

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -7,11 +7,19 @@
       <v-container>
         <router-view />
       </v-container>
+      <v-container>
+        <v-row>
+          <v-col>
+            <app-footer></app-footer>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import AppFooter from './components/AppFooter.vue';
 //
 // import { VApp, VMain, VAppBar, VAppBarNavIcon } from 'vuetify/components';
 
--- ui/src/pages/index.vue
+++ ui/src/pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>Salamasdasd
-    <v-btn :to="{ name: 'about' }">hope</v-btn>
+  <div>
+    <h1>Simotel Loadbalance System </h1>
   </div>
 </template>
 
 <script setup>
-import { VBtn } from 'vuetify/components';
 </script>
