f6b5f1b17cf49dda0d2b212dfe444ad80da832b6
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-08-05 20:01:13 +0330

    Update loadbalance-bottom.php

--- loadbalance-bottom.php
+++ loadbalance-bottom.php
@@ -4,7 +4,7 @@
 var loadbalanceIntervalId = null;
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    loadbalanceIntervalId = setInterval(insertLoadbalanceMenu, 10);
+    loadbalanceIntervalId = setInterval(insertLoadbalanceMenu, 1000);
 
 });
 function insertLoadbalanceMenu(){
