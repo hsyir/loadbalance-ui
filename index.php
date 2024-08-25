1f9c9dca87904286147370435b3a4cef99832f7b
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-08-25 18:50:16 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
 
-require_once __DIR__ . "/../html/pages/maintenance/maintenance.php";
 
 
 
 if(isset($_GET['loadbalance']) and $_GET['loadbalance'] == 'true') {
+    require_once __DIR__ . "/../html/pages/maintenance/maintenance.php";
     $username = Maintenance::getTokenUser($userToken);
-    echo ($username);
     require __DIR__ . '/loader.php';
 }
