fb18120504c3d383ded7cbad16275420aede9de3
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-08-25 20:10:41 +0330

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,11 @@ if(isset($_GET['loadbalance']) and $_GET['loadbalance'] == 'true') {
     $userToken = isset($_GET['token']) ? $_GET['token'] : "";
     $userData = isset($_GET['data']) ? $_GET['data'] : "{}";
 
+    if($_GET["action"]=="asset"){
+      require __DIR__ . '/loader.php';
+      return;      
+    }
+    
     require_once __DIR__ . "/../html/pages/maintenance/maintenance.php";
     if(Maintenance::userHasAccess($userToken, $contentId, $contentType, $userData)) {
         require __DIR__ . '/loader.php';
