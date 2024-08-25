c41f27bda647dd49b7783ebd5dfcc5d331b7ea67
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2024-08-25 19:56:53 +0330

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
 
 
 if(isset($_GET['loadbalance']) and $_GET['loadbalance'] == 'true') {
+
+    $contentId = isset($_POST['id']) ? $_POST['id'] : "";
+    $contentType = isset($_POST['type']) ? $_POST['type'] : "";
+    $userToken = isset($_POST['token']) ? $_POST['token'] : "";
+    $userData = isset($_POST['data']) ? $_POST['data'] : "{}";
+
     require_once __DIR__ . "/../html/pages/maintenance/maintenance.php";
-    $username = Maintenance::getTokenUser($userToken);
-    require __DIR__ . '/loader.php';
+    if(Maintenance::userHasAccess($userToken, $contentId, $contentType, $userData)) {
+        require __DIR__ . '/loader.php';
+    }
+    else{
+      die("403");
+    }
+
 }
